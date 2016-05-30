a64f1cc1f174827fecb24677c421090dcccf2fb6
KKoovalsky <kacper.j.kowalski@student.put.poznan.pl>
2016-05-30 21:35:24 +0200

    Need now to implement PIDs

--- init.c
+++ init.c
@@ -19,6 +19,6 @@ volatile roll_new, pitch_new, yaw_new;
 
 void calc_new_RPY(uint16_t L_x, uint16_t L_y, uint16_t R_x, uint16_t R_y) {
 	yaw_new = (L_x - AXIS_ZERO_LVL) / AXIS_ZERO_LVL * MAX_YAW;
-	pitch_new = (R_x - AXIS_ZERO_LVL) / AXIS_ZERO_LVL * MAX_PITCH;
+	pitch_new = (-1)*(R_x - AXIS_ZERO_LVL) / AXIS_ZERO_LVL * MAX_PITCH;
 	roll_new = (R_y - AXIS_ZERO_LVL) / AXIS_ZERO_LVL * MAX_PITCH;
 }
